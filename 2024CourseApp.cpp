4b73bb180132a93aee581ee61ef3f796adb48cb3
吳塏鈞 <4B3G0048@office.stust.edu.tw>
2024-12-04 17:48:47 +0800

    設定

--- 2024CourseApp.cpp
+++ 2024CourseApp.cpp
@@ -1,20 +1,48 @@
-﻿// 2024CourseApp.cpp : 此檔案包含 'main' 函式。程式會於該處開始執行及結束執行。
-//
-
+﻿#include <string>
 #include <iostream>
+//#include "Person.h"
+#include "Student.h"
+#include "Teacher.h"
+#include "Course.h"
+#include "Utility.h"
+#include "2024CourseApp.h"
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	InitializeData();
 }
 
-// 執行程式: Ctrl + F5 或 [偵錯] > [啟動但不偵錯] 功能表
-// 偵錯程式: F5 或 [偵錯] > [啟動偵錯] 功能表
+void InitializeData()
+{
+	//Person person1("A123456789", "Chen", "Jason", "M", "1999-01-01");
 
-// 開始使用的提示: 
-//   1. 使用 [方案總管] 視窗，新增/管理檔案
-//   2. 使用 [Team Explorer] 視窗，連線到原始檔控制
-//   3. 使用 [輸出] 視窗，參閱組建輸出與其他訊息
-//   4. 使用 [錯誤清單] 視窗，檢視錯誤
-//   5. 前往 [專案] > [新增項目]，建立新的程式碼檔案，或是前往 [專案] > [新增現有項目]，將現有程式碼檔案新增至專案
-//   6. 之後要再次開啟此專案時，請前往 [檔案] > [開啟] > [專案]，然後選取 .sln 檔案
+	//Person* person2 = new Person();
+	//person2->setId("B987654321");
+	//person2->setLastName("Wang");
+	//person2->setFirstName("David");
+	//person2->setGender("M");
+	//person2->setBirthDate("1999-12-31");
+
+	//person1.display();
+	//cout << endl;
+	//person2->display();
+
+	Student student1("A123456789", "陳", "小明", "男", "1999-01-01", "S001", Department::ComputerSciece, ClassName::_1A);
+	student1.display();
+
+	cout << endl;
+	Course course1("C001", "C++ Programming", "這門課程教授C++程式語言");
+	Course course2("C002", "Java Programming", "這門課程教授Java程式語言");
+	Course course3("C003", "Python Programming", "這門課程教授Python程式語言");
+	//course1.display();
+	//course2.display();
+	//course3.display();
+
+	vector<Course> teacher1_courses = { course1, course2, course3 };
+
+	Teacher teacher1("T123456789", "王", "大富", "男", "1980-7-1", "T001", Department::ComputerSciece, ClassName::_1A, teacher1_courses);
+
+	cout << "----------------" << endl;
+	teacher1.display();
+}
---
+++ Teacher.cpp
@@ -0,0 +1,60 @@
+#include "Teacher.h"
+#include <iostream>
+using namespace std;
+
+Teacher::Teacher(const string& id, const string& lastName, const string& firstName, const string& gender, const string birthDate, const string& teacherId, Department department, ClassName, const vector<Course>& courses) : Person(id, lastName, firstName, gender, birthDate), teacherId(teacherId), department(department), className(className), teachingCourses(courses)
+{
+}
+
+string Teacher::getTeacherId() const
+{
+	return teacherId;
+}
+
+Department Teacher::getDepartment() const
+{
+	return department;
+}
+
+ClassName Teacher::getClassName() const
+{
+	return className;
+}
+
+vector<Course> Teacher::getCourses() const
+{
+	return teachingCourses;
+}
+
+void Teacher::setTeacherId(const string& teacherId)
+{
+	this->teacherId = teacherId;
+}
+
+void Teacher::setDepartment(Department department)
+{
+	this->department = department;
+}
+
+void Teacher::setClassName(ClassName className)
+{
+	this->className = className;
+}
+
+void Teacher::setCourses(const vector<Course>& courses)
+{
+	this->teachingCourses = courses;
+}
+
+void Teacher::display() const
+{
+	Person::display();
+	cout << ": " << teacherId << endl;
+	cout << ": " << Utility::toString(department) << endl;
+	cout << ": " << Utility::toString(className) << endl;
+	cout << ": " << endl;
+	for (const Course& course : teachingCourses)
+	{
+		course.display();
+	}
+}
